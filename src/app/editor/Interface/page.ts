882f51c4e52bf225689e137a1ecd085a38f3b994
UtsavGohel <utsavgohel2002@gmail.com>
2025-03-30 21:10:47 +0530

    feat(interface): configure step interface

---
+++ src/app/editor/Interface/page.ts
@@ -0,0 +1,24 @@
+export enum StepType {
+  CreateFile,
+  CreateFolder,
+  EditFile,
+  DeleteFile,
+  RunScript,
+}
+
+export interface Step {
+  id: number;
+  title: string;
+  description: string;
+  type: StepType;
+  status: "pending" | "in-progress" | "completed";
+  code?: string;
+}
+
+export interface FileItem {
+  name: string;
+  type: "file" | "folder";
+  children?: FileItem[];
+  content?: string;
+  path: string;
+}
